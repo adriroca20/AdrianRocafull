946860ee540323c7f2604d27116966d172b7ccb4
adriroca20 <hardhe21@gmail.com>
2022-10-01 00:48:31 +0200

    Añadido mi portfolio

---
+++ resources/scripts/animations.js
@@ -0,0 +1,31 @@
+/***************
+/ SELECTORS
+****************/
+
+const botonNavbar = document.getElementById("botonNavbar");
+const botonNavbarAux= document.getElementById("botonNavbarAux");
+const navBar= document.getElementById("navbarPrincipal");
+const imagenPrueba= document.getElementById("prueba");
+/***************
+/ Listeners
+****************/
+
+
+botonNavbarAux.addEventListener("click",()=>{
+    botonNavbar.classList.toggle("fa-xmark");
+    botonNavbar.classList.toggle("fa-bars");
+});
+
+
+/***************
+/ Funciones
+****************/
+
+
+
+/***************
+/ Ejecuciones
+****************/
+
+//neonBackground();
+
